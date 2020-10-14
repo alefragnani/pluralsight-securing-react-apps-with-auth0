fd124081f9bedd91408ed4499534a16beba52a00
Alessandro Fragnani <alefragnani@hotmail.com>
2020-10-14 00:27:12 -0300

    Add Styling

--- src/Nav.js
+++ src/Nav.js
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 const Nav = () => {
 
   return (
-    <div>
+    <nav>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/profile">Profile</Link></li>
       </ul>
-    </div>
+    </nav>
   );
 }
 
